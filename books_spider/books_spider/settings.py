bfa32b3341acae380d247d102d94cba794e65975
chuck <chuck@chuck-hbase.(none)>
2013-03-27 11:11:21 +0800

    add scrapy files

---
+++ books_spider/books_spider/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for books_spider project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'books_spider'
+
+SPIDER_MODULES = ['books_spider.spiders']
+NEWSPIDER_MODULE = 'books_spider.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'books_spider (+http://www.yourdomain.com)'
