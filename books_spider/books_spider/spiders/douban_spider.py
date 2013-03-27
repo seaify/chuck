5b11ed248828c530a027cde0842344fcb8e372cb
chuck-cug <fengjing.code@gmail.com>
2013-03-27 14:17:30 +0800

    satisfy pep8

--- books_spider/books_spider/spiders/douban_spider.py
+++ books_spider/books_spider/spiders/douban_spider.py
@@ -2,32 +2,29 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy import log
 import re
+import os
 
 
 class douban_spider(CrawlSpider):
 
     name = "douban"
-    
     start_urls = ["http://book.douban.com/tag/"]
 
     allow_domain = "book.douban.com"
 
     rules = (
         Rule(SgmlLinkExtractor(allow=("/tag"))),
-        Rule(SgmlLinkExtractor(allow=("subject/\d+/$")), callback='parse_page'),
+        Rule(SgmlLinkExtractor(allow=("subject/\d+/$")),
+            callback='parse_page'),
         )
 
     def get_url_id(self, response):
-        
-        m = re.search("subject/(\d+)$", response.url)
+        m = re.search("subject/(\d+)/$", response.url)
         if m:
             return m.group(1)
         return None
 
-
     def parse_page(self, response):
-
         url_id = self.get_url_id(response)
         if url_id:
             open(os.path.join('douban/', url_id), 'w').write(response.body)
-        
