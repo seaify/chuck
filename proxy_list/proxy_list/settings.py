0f5a1ac299f48af4eeca0f2236f20b5ffb1c8f89
chuck-cug <fengjing.code@gmail.com>
2013-03-27 21:04:28 +0800

    add proxy list crawler

---
+++ proxy_list/proxy_list/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for proxy_list project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/topics/settings.html
+#
+
+BOT_NAME = 'proxy_list'
+
+SPIDER_MODULES = ['proxy_list.spiders']
+NEWSPIDER_MODULE = 'proxy_list.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'proxy_list (+http://www.yourdomain.com)'
